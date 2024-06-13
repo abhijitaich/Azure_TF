b6e510d05d863b1820ccdd798675d7b490f8f1f9
abhijit <abhijit.aich@live.in>
2024-06-13 11:04:34 +0000

    terraform with cloud

--- provider.tf
+++ provider.tf
@@ -1,9 +1,18 @@
+##first we will modify the provider.tf file and put some entry
+#open the provider.tf file replace all the content with the below mentioned content
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-storage"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "terraformstorageabhi"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
@@ -13,5 +22,25 @@ provider "azurerm" {
   features {}
   #
   # delete_vm_but_do_not_delete_storage
-
+  subscription_id = var.subscription_id 
+  client_id =  var.client_id
+  client_secret =  var.client_secret
+  tenant_id =  var.tenant_id
 }
+
+variable "client_id" {
+  type = string 
+}
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+
